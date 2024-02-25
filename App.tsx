a9a0648ee36d8c355db5d7c167becfd97bdbf92b
weeiwaai <weeiwaai02@gmail.com>
2024-02-25 23:00:58 +1030

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RootLayout from './app/_layout';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RootLayout />;
 }
 
 const styles = StyleSheet.create({
